8c5eece797d3fae30a9586a580e39e00b6f987d1
Arash <arashabedi998@gmail.com>
2021-04-29 10:31:36 +0430

    add user class

--- inc/classes/UserClass.php
+++ inc/classes/UserClass.php
@@ -3,5 +3,14 @@
 //User
 
 class User {
-    
+    private $id;
+    private $firstName;
+    private $lastName;
+    private $phone;
+    private $password;
+    private $email;
+    private $isVarified;
+    private $registerCode;
+    private $createdAt;
+    private $updatedAt;
 }
