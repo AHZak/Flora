3a9879f446ddbfa2b83a6cb6c4d980beef9a9f0f
Arash <arashabedi998@gmail.com>
2021-05-01 11:35:05 +0430

    add and get categories & sub categories

--- http/controller.php
+++ http/controller.php
@@ -1,7 +1,22 @@
 <?php
 
-include_once '../config.php';
+if(isset($pageUi)){
+    if(isset($_POST['addCategory'])){
+        $catName=isset($_POST['name']) ? $_POST['name'] : "";
+        $catType=isset($_POST['catType']) ? $_POST['catType'] : "";
+        
+        if($catType!=""){
+            if($catType=='parent'){
+                //add category
+                Category::create(['name'=>$catName,'creator_id'=>1],$message);
+                $message->showMessages();
+            }else{
+                //add sub category
+                SubCategory::create(['name'=>$catName,'creator_id'=>1,'category_id'=>$catType]);
+            }
+        }
+    }
+    //get categories and sub categories
+    $categories=Category::getCategories();
 
-$ac=new Account();
-$ac->createInitialaizeAccount("09360795096");
-$ac->getMessageHandler()->showMessages();
+}
