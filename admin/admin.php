672f526e78f3ec40044ebe3781958d0581de817d
Amir Zakaria <zakariazade@gmail.com>
2021-05-03 17:45:41 +0430

    fixed adminheader + common adminheader

---
+++ admin/admin.php
@@ -0,0 +1,111 @@
+<?php
+
+include 'adminheader.php';
+
+?>
+
+    <div class="container-fluid">
+  <div class="row">
+
+  <!----------------------------- sidebar --------------------------->
+
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" style="">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active border-bottom" aria-current="page" href="#">
+            <i class="fas fa-home"></i>
+            خانه
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="#">
+            <i class="fas fa-users feather"></i>
+            کاربران
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="#">
+            <i class="fas fa-cart-arrow-down feather"></i>
+            سفارشات
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="#">
+            <i class="fas fa-receipt feather"></i>
+            فاکتورها
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="addproduct.php">
+            <i class="fas fa-plus feather"></i>
+            افزودن محصول
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="products.php">
+            <i class="fas fa-shopping-bag feather"></i>
+            محصولات
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="categories.php">
+            <i class="fas fa-archive feather"></i>
+            دسته بندی ها
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="#">
+            <i class="fas fa-truck feather"></i>
+            تنظیمات ارسال
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="#">
+            <i class="fas fa-comment-alt feather"></i>
+            پنل پیامک
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="#">
+            <i class="fas fa-image feather"></i>
+            اسلاید ها
+            </a>
+          </li>
+          <li class="nav-item border-bottom">
+            <a class="nav-link" href="#">
+            <i class="fas fa-map-marked-alt feather"></i>
+            تنظیمات نقشه
+            </a>
+          </li>
+        </ul>
+        
+      </div>
+    </nav>
+
+    <!----------------------------- left panel --------------------------->
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="container">
+        <div class="row">
+          <!----------------------------- cards --------------------------->
+          <div class="d-flex flex-wrap justify-content-center m-3">
+            <div class="bg-dark rounded tmp-card"></div>
+            <div class="bg-warning rounded tmp-card"></div>
+            <div class="bg-info rounded tmp-card"></div>
+          </div>
+
+        </div>
+      </div>
+    </main>
+  </div>
+</div>
+
+    
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+
+<!-- adding this line of code to test rebase -->
---
+++ admin/adminheader.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html dir="rtl">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.rtl.min.css" integrity="sha384-trxYGD5BY4TyBTvU5H23FalSCYwpLA0vWEvXXGm5eytyztxb+97WzzY+IWDOSbav" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../assets/css/admin.css">
+
+    <title>فلورا</title>
+    <!-- Fontawesome kit -->
+    <script src="https://kit.fontawesome.com/2370aca281.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shodow">
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 py-3 " href="#">پنل ادمین</a>
+      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="تبديل التنقل">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <ul class="navbar-nav px-3 d-flex flex-row align-items-center justify-content-end w-100">
+      <li class="nav-item text-nowrap text-light me-auto">
+          <p class="m-0">
+          یکشنبه 1400/02/12
+          </p>
+      </li>
+      <li class="nav-item text-nowrap">
+          <a class="nav-link mx-2 mb-0" href="#">
+          <i class="fas fa-user px-2"></i>
+          <span>امیر ذکریا</span>
+          </a>
+        </li>
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#"><button type="button" class="btn btn-danger pb-2 mx-2">خروج</button></a>
+        </li>
+      </ul>
+    </header>
+
+    <!----------------------------- the rest goes here --------------------------->
