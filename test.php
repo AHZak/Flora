5d6cc057aa4cf0f0b00c1bacde451cb2e9bca831
Arash <arashabedi998@gmail.com>
2021-05-05 09:08:05 +0430

    updated some stuff

--- test.php
+++ test.php
@@ -5,4 +5,5 @@ include 'config.php';
 // Product::create(['title'=>'tese','image'=>'image.png','price'=>200,'description'=>'its a des','instock'=>10],$message);
 // $message->showMessages();
 
-echo jdate(" Y/m/d l");
+$sub=new Category(3);
+show(Product::getProducts(1,"id","ASC"));
