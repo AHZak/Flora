2d50aea701ffdefb87c06009c58bf051ce44d99d
Amir Zakaria <zakariazade@gmail.com>
2021-05-11 03:14:09 +0430

    carousel resized in index

--- index.php
+++ index.php
@@ -67,13 +67,13 @@
           <div class="carousel-item">
             <!-- <svg class="d-block w-100" width="800" height="400" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
             -->
-            <a href="#"><img src="assets/images/slider/slider-s1.jpg" class="d-block w-100" width="100%", style="max-height:600px"  alt=""></a>
+            <a href="#"><img src="assets/images/slider/slider-s1.jpg" class="d-block w-100" width="100%", style="max-height:500px"  alt=""></a>
           </div>
           <div class="carousel-item">
-            <a href="#"><img src="assets/images/slider/slider-s2.jpg" class="d-block w-100" width="100%", style="max-height:600px"  alt=""></a>
+            <a href="#"><img src="assets/images/slider/slider-s2.jpg" class="d-block w-100" width="100%", style="max-height:500px"  alt=""></a>
           </div>
           <div class="carousel-item active">
-            <a href="#"><img src="assets/images/slider/slider-s3.jpg" class="d-block w-100" width="100%", style="max-height:600px"  alt=""></a>
+            <a href="#"><img src="assets/images/slider/slider-s3.jpg" class="d-block w-100" width="100%", style="max-height:500px"  alt=""></a>
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
