4de9f7ddd9b132a7cd4d61424f29685eba4ffec3
Amir Zakaria <zakariazade@gmail.com>
2021-05-14 18:13:32 +0430

    dynamic sidebar links

--- admin/products.php
+++ admin/products.php
@@ -8,81 +8,9 @@
     <div class="container-fluid">
   <div class="row">
 
-  <!----------------------------- sidebar --------------------------->
-
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" style="">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link border-bottom" aria-current="page" href="#">
-            <i class="fas fa-home"></i>
-            خانه
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="#">
-            <i class="fas fa-users feather"></i>
-            کاربران
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="#">
-            <i class="fas fa-cart-arrow-down feather"></i>
-            سفارشات
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="#">
-            <i class="fas fa-receipt feather"></i>
-            فاکتورها
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="addproduct.php">
-            <i class="fas fa-plus feather"></i>
-            افزودن محصول
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link active" href="products.php">
-            <i class="fas fa-shopping-bag feather"></i>
-            محصولات
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="categories.php">
-            <i class="fas fa-archive feather"></i>
-            دسته بندی ها
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="#">
-            <i class="fas fa-truck feather"></i>
-            تنظیمات ارسال
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="#">
-            <i class="fas fa-comment-alt feather"></i>
-            پنل پیامک
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="#">
-            <i class="fas fa-image feather"></i>
-            اسلاید ها
-            </a>
-          </li>
-          <li class="nav-item border-bottom">
-            <a class="nav-link" href="#">
-            <i class="fas fa-map-marked-alt feather"></i>
-            تنظیمات نقشه
-            </a>
-          </li>
-        </ul>
-        
-      </div>
-    </nav>
+<?php
+  include 'adminsidebar.php'
+?>
 
     <!----------------------------- left panel --------------------------->
 
