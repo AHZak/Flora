a768aa96fe3d66f8bddd98f4e196205b74823397
Amir Zakaria <zakariazade@gmail.com>
2021-06-19 02:00:13 +0430

    GeoJSON + some additions

---
+++ admin/geojson.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once '../config.php';
+
+//AUTH
+if(isAdmin() || isMaster()){
+  include 'adminheader.php';
+
+?>
+
+    <div class="container-fluid">
+    <div class="row">
+
+    <?php
+    include 'adminsidebar.php'
+
+    ?>
+
+      <!----------------------------- left panel --------------------------->
+
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="container">
+          <div class="row p-3">
+            <div class="col-12 border border-3">
+              <div class="d-flex flex-column p-3 align-items-center justify-content-between" style="height: 40vh">
+                <p class="h2">GeoJSON</p>
+                <p>فایل خروجی گرفته از وبسایت <a href="https://geojson.io/" target="__blank">Geojson.io</a> را بارگزاری نمایید.</p>
+                <div class="">
+                  <input class="form-control form-control-lg" id="formFileLg" type="file">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+
+      
+      <!-- Option 1: Bootstrap Bundle with Popper -->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+      <!--JQuery cdn -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="../assets/js/extra.js"></script>   
+    </body>
+  </html>
+
+  <!-- adding this line of code to test rebase -->
+<?php }else {
+  echo "Access Denied!";
+} ?>
