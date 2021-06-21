02aeeb1c0cd446cad672c6be20c0790a553b7d56
Amir Zakaria <zakariazade@gmail.com>
2021-06-21 21:13:08 +0430

    fixes: admin panel

--- admin/adminheader.php
+++ admin/adminheader.php
@@ -17,7 +17,7 @@
   <body>
 
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shodow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 py-3 " href="#"><img src="../assets/images/logo/flora-lo.png" height="25px" class="me-3" alt=""> پنل ادمین</a>
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 py-3 " href="../index.php" target="__blank"><img src="../assets/images/logo/flora-lo.png" height="25px" class="me-3" alt=""> پنل ادمین</a>
       <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="تبديل التنقل">
         <span class="navbar-toggler-icon"></span>
       </button>
